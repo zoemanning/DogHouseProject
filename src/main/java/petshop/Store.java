603361f1c03ff8c09e55cc7cc4d8db91edaaae7c
Oludolapo Adepoju-Grace <dolly.ograce@oludolapos-air.myfiosgateway.com>
2022-02-24 14:01:08 -0500

    Added Customer and Store

--- src/main/java/petshop/Store.java
+++ src/main/java/petshop/Store.java
@@ -1,4 +1,34 @@
 package petshop;
 
-public class Store {
+import java.util.ArrayList;
+
+public class Store<E, D> {
+
+    private ArrayList<E> employees;
+    private ArrayList<D> dogs;
+
+    public Store() {
+        employees = new ArrayList<>();
+        dogs = new ArrayList<>();
+    }
+
+
+    public void hireEmployee(E employee) {
+        employees.add(employee);
+    }
+
+    public D sell() throws Exception {
+        if (dogs.isEmpty())
+            throw new Exception("No more inventory");
+        return dogs.remove(0);
+    }
+
+    public void addDogs(D item) {
+
+        dogs.add(item);
+    }
+
+
 }
+
+
