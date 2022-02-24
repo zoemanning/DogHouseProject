f151672af8b3e51dc9b4fa5a52ef4865e4a83a26
Oludolapo Adepoju-Grace <dolly.ograce@oludolapos-air.myfiosgateway.com>
2022-02-24 10:39:22 -0500

    first Commit

--- src/main/java/people/Person.java
+++ src/main/java/people/Person.java
@@ -1,4 +1,32 @@
 package people;
 
 public abstract class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+        public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "fullName='" + getFullName() +  '}';
+    }
+
+
 }
+
+
