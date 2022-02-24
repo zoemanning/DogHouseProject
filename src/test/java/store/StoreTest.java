e3d0de49830b24dca5873a21d8d676bfb1a21d79
Oludolapo Adepoju-Grace <dolly.ograce@oludolapos-air.myfiosgateway.com>
2022-02-24 15:28:01 -0500

    Added one test

--- src/test/java/store/StoreTest.java
+++ src/test/java/store/StoreTest.java
@@ -1,17 +1,23 @@
 package store;
 
+import dogs.BarkLevel;
+import dogs.EnergyLevel;
+import dogs.HairType;
+import dogs.Size;
+import dogs.breeds.BichonFrise;
 import dogs.breeds.Boxer;
-import org.testng.annotations.Test;
-import people.Customer;
+import org.junit.jupiter.api.Test;
 import people.Employee;
+import people.Job;
 import petshop.Store;
 
 public class StoreTest {
 
     @Test
-    public void addDogTest(){
-        Employee<Boxer>  = new Employee<>("Dolly", "Jordan", new Boxer());
-        Store<Employee<Boxer>, Boxer> store = new Store<>();
-        store.addDogs(Boxer);
+    public void hireEmployeeTest(){
+        Employee<BichonFrise> employee = new Employee<>("Joe", "Jackson", 1, Job.CASHIER, new BichonFrise("Bella", 2000.00, EnergyLevel.LOW, BarkLevel.SMALL_BARK, HairType.HAIRLESS, Size.SMALL)); ;
+        Store<Employee<BichonFrise>, BichonFrise> store = new Store<>();
+        store.hireEmployee(employee);
     }
+
 }
